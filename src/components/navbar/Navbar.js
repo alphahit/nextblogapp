c07e4937b9b0d8d3020ea241586ec034a8714e25
Prateek Priyadarshi <prateekpriyadarshi1328@gmail.com>
2023-12-31 22:09:38 +0530

    First Commit Setting Up Templates

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,13 @@
+import Links from "./links/Links";
+
+export default function Navbar() {
+  return (
+    <div>
+      <div>LOGO</div>
+      <div>
+      <Links/>
+      </div>
+     
+      </div>
+  )
+}
